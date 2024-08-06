513abb4db67e368c3830c7cf388e8a754bb99779
Ficus <s.savchenko@tg.dunice.net>
2024-08-06 14:33:34 +0300

    first commit

---
+++ migrations/20240805114949-create-user.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      taskText: {
+        type: Sequelize.STRING,
+      },
+      isChecked: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Tasks');
+  },
+};
